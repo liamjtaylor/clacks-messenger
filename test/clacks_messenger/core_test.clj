e222c86221109e74812102c0874d1f5d84f118d3
liamjtaylor <liam.taylor@uswitch.com>
2016-03-07 19:46:53 +0000

    initial project

---
+++ test/clacks_messenger/core_test.clj
@@ -0,0 +1,7 @@
+(ns clacks-messenger.core-test
+  (:require [clojure.test :refer :all]
+            [clacks-messenger.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
