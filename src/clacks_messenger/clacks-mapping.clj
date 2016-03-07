24c13536cb79fbeb64ebb0b7011f7d982c203036
liamjtaylor <liam.taylor@uswitch.com>
2016-03-07 21:08:09 +0000

    initial mapping and build clacks interpreter

---
+++ src/clacks_messenger/clacks-mapping.clj
@@ -0,0 +1,40 @@
+(ns clacks-messenger.clacks-mapping
+  (require [clojure.string :as clj-str]))
+
+(def alphabet {:a [0,1,0,0,0,1]
+               :b [0,0,1,0,1,0]
+               :t [1,0,0,1,1,1]
+               :. [1,0,1,1,0,1]
+               :- [0,0,1,0,0,0]})
+
+(defn reverse-map [m]
+  (into {} (map (fn [[a b]] [b a]) m)))
+
+(defn kebabify [string]
+  (clj-str/replace string " " "-"))
+
+(defn de-kebabify [string]
+  (clj-str/replace string "-" " "))
+
+(defn clacksify [letter]
+  (let [character (str letter)]
+  (alphabet (keyword character))))
+
+(defn back-to-letter [x] (name ((reverse-map alphabet) x)))
+
+(defn string->clacks [word]
+  (->> word
+       kebabify
+       (map clacksify)))
+
+(defn clacks->string [clacks]
+  (->> clacks
+       (map back-to-letter)
+       (reduce str)
+       de-kebabify))
+
+(def returned (string->clacks "bt bt."))
+
+(clacks->string returned)
+
+(string->clacks " bt.")
