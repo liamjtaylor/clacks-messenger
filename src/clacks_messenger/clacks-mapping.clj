c04015425929573bc579e1714d5ffb4b6f5abefc
liamjtaylor <liam.taylor@uswitch.com>
2016-03-11 09:04:41 +0000

    using 8 verctor binary for alphabet, tidied up format

--- src/clacks_messenger/clacks-mapping.clj
+++ src/clacks_messenger/clacks-mapping.clj
@@ -1,34 +1,6 @@
 (ns clacks-messenger.clacks-mapping
-  (require [clojure.string :as clj-str]))
-
-(def alphabet {:a [0,1,0,0,0,1]
-               :b [0,0,1,0,1,0]
-               :c [0,1,0,0,1,0]
-               :d [1,0,1,0,0,0]
-               :e [1,0,1,1,0,0]
-               :f [1,1,0,1,0,0]
-               :g [1,0,0,1,1,0]
-               :h [1,0,1,0,0,1]
-               :i [1,1,1,0,0,0]
-               :j [0,0,1,1,1,1]
-               :k [0,1,0,1,0,1]
-               :l [1,1,1,0,0,1]
-               :m [1,1,1,0,1,1]
-               :n [0,1,1,1,0,1]
-               :o [1,1,0,1,1,0]
-               :p [1,1,1,1,1,0]
-               :q [1,0,1,1,1,0]
-               :r [1,1,1,1,0,0]
-               :s [0,1,1,1,1,0]
-               :t [1,0,0,1,1,1]
-               :u [0,0,1,0,1,1]
-               :v [0,1,1,0,0,1]
-               :w [1,1,0,1,0,1]
-               :x [1,0,1,0,1,0]
-               :y [1,0,0,0,1,1]
-               :z [1,1,0,0,1,1]
-               :. [1,0,1,1,0,1]
-               :- [0,0,1,0,0,0]})
+  (require [clojure.string :as clj-str]
+           [clacks-messenger.alphabet-mapping :as alph-map]))
 
 (defn reverse-map [m]
   (into {} (map (fn [[a b]] [b a]) m)))
@@ -41,10 +13,10 @@
 
 (defn clacksify [letter]
   (let [character (str letter)]
-  (alphabet (keyword character))))
+  (alph-map/alphabet (keyword character))))
 
 (defn back-to-letter [clack]
-  (name ((reverse-map alphabet) clack)))
+  (name ((reverse-map alph-map/alphabet) clack)))
 
 (defn string->clacks [word]
   (->> word
@@ -57,9 +29,9 @@
        (reduce str)
        de-kebabify))
 
-(string->clacks "hello this is the message.")
+(string->clacks "Hello this is the message!!!")
 
-(def returned (string->clacks "hello this is the message."))
+(def returned (string->clacks "Hello @£%&^<> this is the message!!??!??!?"))
 
 (clacks->string returned)
 
